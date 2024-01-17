f73d99b99111a63b8b21e0b3d9358aacb81ef651
Katerina <katen-91@mail.ru>
2024-01-17 19:52:19 +0300

    auth1

--- src/components/components.js
+++ src/components/components.js
@@ -1,23 +1,17 @@
 import IndexPage from './pages/IndexPage';
 import OneTrackPage from './pages/OneTrackPage';
-<<<<<<< HEAD
 import HomePage from './pages/HomePage';
 import ProfilePage from './pages/ProfilePage';
-=======
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
->>>>>>> 519992c (singUpPagr)
 
 const components = {
   IndexPage,
   OneTrackPage,
-<<<<<<< HEAD
   HomePage,
   ProfilePage,
-=======
   SignUpPage,
   LoginPage,
->>>>>>> 519992c (singUpPagr)
 };
 
 export default components;
